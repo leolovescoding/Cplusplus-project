aee9213fe8cdf5eba01216dcdd38e2cde245afde
leolovescoding <123299184+leolovescoding@users.noreply.github.com>
2023-03-27 16:53:35 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+   //<iostream> header allows us to access input output functions 
+   
+#include <fstream>
+   //fstream header allows us to open and work with files
+   
+#include <sstream>
+   //sstream header provides templates for interoperations between stream buffers and string objects
+   
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // creating variables
+    int s, i=0;
+    string text,password0,password1,password2,pass,old,name,age,user,word,word1;
+    string cred[2],cp[2];
+    
+    
+
+
+
+
+
+
+
+}
+
+
